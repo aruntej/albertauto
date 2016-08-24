36a99d8afc8bfab6d181165b137eda74b4dbc1c8
Arun Seelam <aseelam@aseelam-macbookpro.roam.corp.google.com>
2016-08-23 23:05:58 -0700

    Vijay updated

--- Albertauto/mobile/src/main/java/com/einsteinauto/albertalpha/albertauto/FanSpeed.java
+++ Albertauto/mobile/src/main/java/com/einsteinauto/albertalpha/albertauto/FanSpeed.java
@@ -1,13 +1,22 @@
 package com.einsteinauto.albertalpha.albertauto;
 
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbDevice;
+
+
 public class FanSpeed extends AppCompatActivity {
+    UsbDeviceConnection connection = new UsbDeviceConnection(<Create Device-connection>);
+    UsbDevice device = new UsbDevice(<create device here>)
+    MyDriver myDriver = new MyDriver(<Device>);
+
+    byte [] readArray = new byte[200];
+    byte [] writeArray = new byte[200];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,14 @@ public class FanSpeed extends AppCompatActivity {
         String output_message = AC_Status + " Incresed Fan Speed";
         FanAction.setText(output_message);
 
+        myDriver.open(connection);
+        myDriver.read(readArray, 1000);
+        myDriver.write(writeArray, 1000);
+        myDriver.close();
+
+
+
+
     }
     public void onClickDecreaseFanSpeed(View view){
         TextView FanAction = (TextView) findViewById(R.id.FanAction);
@@ -28,6 +45,11 @@ public class FanSpeed extends AppCompatActivity {
         String AC_Status = String.valueOf(AirCon.getSelectedItem());
         String output_message = AC_Status + " Decreased Fan Speed";
         FanAction.setText(output_message);
+
+        myDriver.open(connection);
+        myDriver.read(readArray, 1000);
+        myDriver.write(writeArray, 1000);
+        myDriver.close();
 
     }
 }
